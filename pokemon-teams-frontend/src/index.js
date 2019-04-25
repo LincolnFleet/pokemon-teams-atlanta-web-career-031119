70312fae4d5cae33933631c5b0b2c7be706b392b
github username <github email address>
2019-04-25 13:54:19 -0400

    pseudocode

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,13 @@
+// render all trainers
+    // render pokemon teams for each trainer
+
+// add pokemon button
+    // add pokemon to team if team.length < 6
+
+// each pokemon needs release button
+    // remove from team => delete?
+
+
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
